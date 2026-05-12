31b26ff8a5938aab8e2d3a0ec3e3c90407fe244e
ekaznyra <103523312+ekaznyra@users.noreply.github.com>
2026-05-13 02:21:01 +0700

    fix(spotify-json): add error handling and UA early-return guard

--- Module/js/spotify-json.js
+++ Module/js/spotify-json.js
@@ -1,28 +1,48 @@
 /*
  * @name: Spotify Premium JSON Handler
  * @author: Nguyễn Ngọc Anh Tú (z3rokaze) - Based on app2smile
- * @homepage: https://github.com/z3rokaze/NguyenNgocAnhTu
- * @date: 2026-05-09
+ * @homepage: https://github.com/ekaznyra/NguyenNgocAnhTu
+ * @date: 2026-05-13
  */
 
-const header = $request.headers;
-const ua = header["user-agent"] || header["User-Agent"];
-let obj = JSON.parse($response.body);
-
-if (ua.includes("Spotify")) {
-    if ($request.url.includes("/artistview/")) {
-        obj.creator_about = {
-            ...obj.creator_about,
-            monthly_listeners: 999999999,
-            following_count: 999999999
-        };
+(function () {
+    const body = $response && $response.body;
+    if (!body) {
+        $done({});
+        return;
     }
-    if ($request.url.includes("/album-entity-view/")) {
-        obj.album_metadata = {
-            ...obj.album_metadata,
-            is_premium_only: false
-        };
-    }
-}
 
-$done({ body: JSON.stringify(obj) });
+    const headers = ($request && $request.headers) || {};
+    const ua = headers["user-agent"] || headers["User-Agent"] || "";
+    const url = ($request && $request.url) || "";
+
+    if (!ua.includes("Spotify")) {
+        $done({});
+        return;
+    }
+
+    let obj;
+    try {
+        obj = JSON.parse(body);
+    } catch (e) {
+        $done({});
+        return;
+    }
+
+    try {
+        if (url.includes("/artistview/")) {
+            obj.creator_about = Object.assign({}, obj.creator_about, {
+                monthly_listeners: 999999999,
+                following_count: 999999999
+            });
+        }
+        if (url.includes("/album-entity-view/")) {
+            obj.album_metadata = Object.assign({}, obj.album_metadata, {
+                is_premium_only: false
+            });
+        }
+        $done({ body: JSON.stringify(obj) });
+    } catch (e) {
+        $done({});
+    }
+})();
