a83fac7e998edb45288c8bd5bc8b84686d863eac
ekaznyra <103523312+ekaznyra@users.noreply.github.com>
2026-05-13 02:27:33 +0700

    revert(spotify): restore upstream app2smile spotify-json.js (URL rewriter, http-request)

--- Module/js/spotify-json.js
+++ Module/js/spotify-json.js
@@ -1,48 +1,18 @@
 /*
- * @name: Spotify Premium JSON Handler
- * @author: Nguyễn Ngọc Anh Tú (z3rokaze) - Based on app2smile
- * @homepage: https://github.com/ekaznyra/NguyenNgocAnhTu
- * @date: 2026-05-13
+ * @name: Spotify JSON URL Rewriter
+ * @desc: Switch platform=iphone → platform=ipad for artistview / album-entity-view
+ *        so the iPad layout is returned (shows full artist/album listings).
+ * @source: https://github.com/app2smile/rules/blob/master/js/spotify-json.js
+ * @type: http-request (URL rewrite only, does NOT read body)
  */
-
-(function () {
-    const body = $response && $response.body;
-    if (!body) {
-        $done({});
-        return;
-    }
-
-    const headers = ($request && $request.headers) || {};
-    const ua = headers["user-agent"] || headers["User-Agent"] || "";
-    const url = ($request && $request.url) || "";
-
-    if (!ua.includes("Spotify")) {
-        $done({});
-        return;
-    }
-
-    let obj;
-    try {
-        obj = JSON.parse(body);
-    } catch (e) {
-        $done({});
-        return;
-    }
-
-    try {
-        if (url.includes("/artistview/")) {
-            obj.creator_about = Object.assign({}, obj.creator_about, {
-                monthly_listeners: 999999999,
-                following_count: 999999999
-            });
-        }
-        if (url.includes("/album-entity-view/")) {
-            obj.album_metadata = Object.assign({}, obj.album_metadata, {
-                is_premium_only: false
-            });
-        }
-        $done({ body: JSON.stringify(obj) });
-    } catch (e) {
-        $done({});
-    }
-})();
+console.log(`spotify-json-2025.06.20`);
+let url = $request.url;
+if (url.includes('com:443')) {
+    url = url.replace(/com:443/, 'com');
+}
+if (url.includes('platform=iphone')) {
+    url = url.replace(/platform=iphone/, 'platform=ipad');
+} else {
+    console.log('无需处理');
+}
+$done({ url });
